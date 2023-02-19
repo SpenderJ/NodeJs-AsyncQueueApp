b91ba3752a7d5da0b0a6cd8834b765c675146d0a
Jules Spender <pro.jspender@gmail.com>
2023-02-19 17:50:55 +0100

    Implement swagger for api-spec

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import router from './routes/messageRoutes';
 import { ConfirmationQueue } from './utils/queue';
+import swaggerUi from 'swagger-ui-express';
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -14,7 +15,13 @@ app.use(express.json());
 app.use(router);
 
 export const messageQueue = new ConfirmationQueue(10000);
+const yaml = require('js-yaml');
+const fs = require('fs');
 
+const swaggerYaml = fs.readFileSync('./src/doc/messageControllerDoc.yaml', 'utf8')
+const swaggerSpec = yaml.load(swaggerYaml);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
